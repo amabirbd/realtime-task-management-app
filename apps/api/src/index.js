d9df656cf137189d96cd521672d2cd9aadbc7af3
abir <amabirbd@gmail.com>
2026-05-01 16:22:24 +0600

    fix

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -27,10 +27,27 @@ const corsOrigin = (origin, callback) => {
     return callback(null, true);
   }
 
-  return callback(new Error(`Origin ${origin} is not allowed by CORS`));
+  return callback(null, false);
 };
 
 // CORS configuration
+app.use((req, res, next) => {
+  const origin = req.headers.origin?.replace(/\/$/, '');
+
+  if (origin && uniqueAllowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Vary', 'Origin');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  }
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 app.use(cors({
   origin: corsOrigin,
   credentials: true,
